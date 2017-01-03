fff24a9e4e8d20a684ec382c4d26e7837632cb93
Tom Clancy <tclancy@gmail.com>
2017-01-03 11:53:16 -0500

    Logging, label handling

--- readmail.py
+++ readmail.py
@@ -1,10 +1,29 @@
+import argparse
 import gmail
+import logging
+import sys
+
+# TODO: set to WARN by default once done with development
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger("Gmail Reader")
 
 try:
     from credentials import username, password
 except ImportError:
-    print "You need to create a file named 'credentials.py' in this folder with two variables: username and password"
+    logger.error(
+        "You need to create a file named 'credentials.py' in this folder with two variables: username and password")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("labels", nargs="*")
+    parser.add_argument("-v", dest="verbose", action="store_true")
+    args = parser.parse_args()
+    if args.verbose:
+        logger.level = logging.DEBUG
     g = gmail.login(username, password)
-    print g.inbox().mail()
+    for lbl in args.labels:
+        logger.info("Opening mail in %s", lbl)
+        try:
+            print g.label(lbl).mail()
+        except AttributeError:
+            logger.error("'%s' does not appear to be a valid label", lbl)
