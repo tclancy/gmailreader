b42ca9e5762f6729b79d7ae5b79cb644a695ff36
Tom Clancy <tclancy@gmail.com>
2017-01-03 13:03:46 -0500

    Output to CSV

--- readmail.py
+++ readmail.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import gmail
 import logging
 from nltk.corpus import stopwords
@@ -15,6 +16,35 @@ try:
 except ImportError:
     logger.error(
         "You need to create a file named 'credentials.py' in this folder with two variables: username and password")
+
+
+def get_bodies_from_label(label):
+    logger.info("Opening mail in %s", label)
+    all_mail = []
+    mail_bodies = []
+    try:
+        all_mail = g.label(label).mail(prefetch=True)
+    except AttributeError:
+        logger.error("'%s' does not appear to be a valid label-- please check the spelling and case", label)
+    for email in all_mail:
+        logger.info("Reading %s", email.subject)
+        body = email.body
+        if body:
+            mail_bodies.append(body)
+    return mail_bodies
+
+
+def write_counts_to_csv(bodies, filename):
+    all_email_bodies = TextBlob(" ".join(bodies))
+    interesting = {word: count for word, count in all_email_bodies.word_counts.items() if word not in stops}
+    sorted_counts = sorted(interesting.items(), key=operator.itemgetter(1), reverse=True)
+    filename = "%s.csv" % filename
+    with open(filename, "wb") as f:
+        logger.warn("Writing to %s", filename)
+        writer = csv.writer(f)
+        writer.writerow(["Word", "Count"])
+        for word, count in sorted_counts:
+            writer.writerow([word, count])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -33,20 +63,7 @@ if __name__ == "__main__":
         logger.warn("Need to download NLTK data for stopwords. This should only happen the first time you run me.")
         stops = set(stopwords.words("english"))
 
-    for lbl in args.labels:
-        logger.info("Opening mail in %s", lbl)
-        all_mail = []
-        try:
-            all_mail = g.label(lbl).mail(prefetch=True)
-        except AttributeError:
-            logger.error("'%s' does not appear to be a valid label", lbl)
-        for email in all_mail:
-            logger.info("Reading %s", email.subject)
-            body = email.body
-            if body:
-                mail_bodies.append(body)
-    all_email_bodies = TextBlob(" ".join(mail_bodies))
-    interesting = {word: count for word, count in all_email_bodies.word_counts.items() if word not in stops}
-    sorted_counts = sorted(interesting.items(), key=operator.itemgetter(1), reverse=True)
-    print sorted_counts
+    for label in args.labels:
+        mail_bodies += get_bodies_from_label(label)
+    write_counts_to_csv(mail_bodies, "-".join(args.labels))
     g.logout()
